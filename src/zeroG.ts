123ae13caeaed698f9dcf3c5b9c111e09f4311d6
benduran <stratodyne@gmail.com>
2020-01-26 10:04:01 -0800

    removed tslint
    updated main zero-g file to follow the new eslint / tsc rules

--- src/zeroG.ts
+++ src/zeroG.ts
@@ -7,26 +7,26 @@ enum Orientation {
   Square,
 }
 
-export interface IPannerOptions {
+export interface PannerOptions {
   changeCursorOnPan?: boolean;
   disabled?: boolean;
   refitOnResize?: boolean;
 
   onScaleChange?: (currentScale: number) => void;
-  onPanEnd?: (panEvent: IPanEvent, instance: ZeroGInstance) => void;
-  onPanMove?: (panEvent: IPanEvent, instance: ZeroGInstance) => void;
-  onPanStart?: (panEvent: IPanEvent, instance: ZeroGInstance) => void;
+  onPanEnd?: (panEvent: PaneEvent, instance: ZeroGInstance) => void;
+  onPanMove?: (panEvent: PaneEvent, instance: ZeroGInstance) => void;
+  onPanStart?: (panEvent: PaneEvent, instance: ZeroGInstance) => void;
 }
 
-const defaultPannerOptions: IPannerOptions = {
+const defaultPannerOptions: PannerOptions = {
   changeCursorOnPan: true,
   disabled: false,
   refitOnResize: true,
 
-  onScaleChange: (currentScale: number) => undefined,
-  onPanEnd: (panEvent: IPanEvent, instance: ZeroGInstance) => undefined,
-  onPanMove: (panEvent: IPanEvent, instance: ZeroGInstance) => undefined,
-  onPanStart: (panEvent: IPanEvent, instance: ZeroGInstance) => undefined,
+  onScaleChange: () => undefined,
+  onPanEnd: () => undefined,
+  onPanMove: () => undefined,
+  onPanStart: () => undefined,
 };
 
 const allowedPannerOptionKeys = Object.keys(defaultPannerOptions);
@@ -35,16 +35,16 @@ export class ZeroGInstance {
   private lastX: number | null = null;
   private lastY: number | null = null;
   private zoom: number | null = null;
-  private mousedown: boolean = false;
-  private hasLoadHandler: boolean = false;
-  private windowResizeTimeout: any;
+  private mousedown = false;
+  private hasLoadHandler = false;
+  private windowResizeTimeout: NodeJS.Timeout | void;
 
   private naturalHeight: number;
   private naturalWidth: number;
   private orientation: Orientation;
   private parent: HTMLElement;
 
-  constructor(private element: HTMLElement, private options: IPannerOptions = defaultPannerOptions, private controlledByDockingProcedure: boolean = false) {
+  constructor(private element: HTMLElement, private options: PannerOptions = defaultPannerOptions, private controlledByDockingProcedure: boolean = false) {
     this.options = { ...defaultPannerOptions, ...options };
     this.init();
   }
@@ -53,7 +53,7 @@ export class ZeroGInstance {
     return Math.min(this.element.clientHeight / this.naturalHeight, this.element.clientWidth / this.naturalWidth);
   }
 
-  private bindHandlers() {
+  private bindHandlers(): void {
     if (this.element instanceof HTMLImageElement || this.element instanceof HTMLVideoElement) {
       this.hasLoadHandler = true;
       this.element.addEventListener('load', this.handleInitialLoad);
@@ -65,7 +65,7 @@ export class ZeroGInstance {
     if (this.options.refitOnResize) window.addEventListener('resize', this.handleWindowResize);
   }
 
-  private unbindHandlers() {
+  private unbindHandlers(): void {
     if (this.hasLoadHandler) {
       this.hasLoadHandler = false;
       this.element.removeEventListener('load', this.handleInitialLoad);
@@ -75,7 +75,7 @@ export class ZeroGInstance {
     document.removeEventListener('mousemove', this.handleMouseMove);
   }
 
-  private computeNaturalDimensions() {
+  private computeNaturalDimensions(): void {
     if (this.element instanceof HTMLImageElement) {
       this.naturalHeight = this.element.naturalHeight;
       this.naturalWidth = this.element.naturalWidth;
@@ -88,8 +88,9 @@ export class ZeroGInstance {
     }
   }
 
-  private init() {
-    this.parent = this.element.parentElement!;
+  private init(): void {
+    if (!this.element.parentElement) throw new Error('Invalid DOM structure provided. Parent element must not be null.');
+    this.parent = this.element.parentElement;
     this.computeNaturalDimensions();
     if (this.naturalWidth > this.naturalHeight) this.orientation = Orientation.Landscape;
     else if (this.naturalHeight > this.naturalWidth) this.orientation = Orientation.Portrait;
@@ -101,31 +102,31 @@ export class ZeroGInstance {
     this.queueInitialFit();
   }
 
-  private fitLandscape() {
+  private fitLandscape(): void {
     this.element.style.width = '100%';
     this.element.style.height = 'auto';
     this.element.style.left = toPx(0);
     this.element.style.top = toPx((this.parent.clientHeight - this.element.clientHeight) / 2);
   }
 
-  private fitPortrait() {
+  private fitPortrait(): void {
     this.element.style.width = 'auto';
     this.element.style.height = '100%';
     this.element.style.left = toPx((this.parent.clientWidth - this.element.clientWidth) / 2);
     this.element.style.top = toPx(0);
   }
 
-  private adjustIfOverflown() {
+  private adjustIfOverflown(): void {
     if (this.element.clientHeight > this.parent.clientHeight) return this.fitPortrait();
     if (this.element.clientWidth > this.parent.clientWidth) return this.fitLandscape();
   }
 
-  private swapMouseCursor() {
+  private swapMouseCursor(): void {
     if (this.mousedown) this.element.style.cursor = 'grabbing';
     else this.element.style.cursor = 'grab';
   }
 
-  private doPan(pageX: number, pageY: number, lastX: number | null = this.lastX, lastY: number | null = this.lastY) {
+  private doPan(pageX: number, pageY: number, lastX: number | null = this.lastX, lastY: number | null = this.lastY): void {
     const deltaX = lastX !== null ? pageX - lastX : 0;
     const deltaY = lastY !== null ? pageY - lastY : 0;
     this.element.style.top = toPx(this.element.offsetTop + deltaY);
@@ -134,41 +135,41 @@ export class ZeroGInstance {
     this.lastY = pageY;
   }
 
-  private preventDrag = (e: DragEvent) => {
+  private preventDrag = (e: DragEvent): void => {
     e.preventDefault();
   }
 
-  private handleMouseDown = (e: MouseEvent) => {
+  private handleMouseDown = (e: MouseEvent): void => {
     this.mousedown = true;
     if (this.options.changeCursorOnPan) this.swapMouseCursor();
     if (this.options.onPanStart) this.options.onPanStart({ lastX: null, lastY: null, x: e.pageX, y: e.pageY }, this);
   }
 
-  private handleMouseUp = (e: MouseEvent) => {
+  private handleMouseUp = (e: MouseEvent): void => {
     if (this.options.onPanEnd) this.options.onPanEnd({ lastX: this.lastX, lastY: this.lastY, x: e.pageX, y: e.pageY }, this);
     this.clearLast();
     this.mousedown = false;
     if (this.options.changeCursorOnPan) this.swapMouseCursor();
   }
 
-  private handleMouseMove = (e: MouseEvent) => {
+  private handleMouseMove = (e: MouseEvent): void => {
     if (this.mousedown) {
       if (this.options.onPanMove) this.options.onPanMove({ lastX: this.lastX, lastY: this.lastY, x: e.pageX, y: e.pageY }, this);
       if (!this.controlledByDockingProcedure) this.doPan(e.pageX, e.pageY);
     }
   }
 
-  private handleWindowResize = () => {
+  private handleWindowResize = (): void => {
     if (this.windowResizeTimeout) this.windowResizeTimeout = clearTimeout(this.windowResizeTimeout);
     this.windowResizeTimeout = setTimeout(() => this.zoomFit(), 1);
   }
 
-  private handleInitialLoad = () => {
+  private handleInitialLoad = (): void => {
     this.computeNaturalDimensions();
     this.fit();
   }
 
-  private fit() {
+  private fit(): void {
     switch (this.orientation) {
       case Orientation.Landscape:
         this.fitLandscape();
@@ -186,22 +187,22 @@ export class ZeroGInstance {
     if (this.options.onScaleChange) this.options.onScaleChange(this.currentScale);
   }
 
-  private queueInitialFit() {
+  private queueInitialFit(): void {
     if (this.element instanceof HTMLImageElement || this.element instanceof HTMLVideoElement) {
       // this.element.onload = this.handleInitialLoad;
       this.element.addEventListener('load', this.handleInitialLoad);
     }
   }
 
-  public destroy() {
+  public destroy(): void {
     this.unbindHandlers();
   }
 
-  public controlledPan(panEvent: IPanEvent) {
+  public controlledPan(panEvent: PaneEvent): void {
     this.pan(panEvent.x, panEvent.y, panEvent.lastX, panEvent.lastY);
   }
 
-  public set<K extends keyof IPannerOptions>(prop: K, val: IPannerOptions[K], reinit: boolean = false) {
+  public set<K extends keyof PannerOptions>(prop: K, val: PannerOptions[K], reinit = false): void {
     if (allowedPannerOptionKeys.indexOf(prop) > -1) {
       this.options[prop] = val;
       if (reinit) {
@@ -211,12 +212,12 @@ export class ZeroGInstance {
     }
   }
 
-  public zoomFit() {
+  public zoomFit(): void {
     this.zoom = null;
     this.fit();
   }
 
-  public zoomInOut(level: number) {
+  public zoomInOut(level: number): void {
     this.zoom = level;
     const newHeight = this.naturalHeight * this.zoom;
     const newWidth = this.naturalWidth * this.zoom;
@@ -225,31 +226,31 @@ export class ZeroGInstance {
     if (this.options.onScaleChange) this.options.onScaleChange(this.currentScale);
   }
 
-  public pan(x: number, y: number, lastX: number | null, lastY: number | null) {
+  public pan(x: number, y: number, lastX: number | null, lastY: number | null): void {
     this.doPan(x, y, lastX, lastY);
   }
 
-  public clearLast() {
+  public clearLast(): void {
     this.lastX = this.lastY = null;
   }
 
-  public getElement() {
+  public getElement(): HTMLElement {
     return this.element;
   }
 
-  public getParent() {
+  public getParent(): HTMLElement {
     return this.parent;
   }
 }
 
-export interface IPanEvent {
+export interface PaneEvent {
   x: number;
   y: number;
   lastX: number | null;
   lastY: number | null;
 }
 
-export default function createZeroG(element: HTMLElement, options: IPannerOptions = defaultPannerOptions) {
+export default function createZeroG(element: HTMLElement, options: PannerOptions = defaultPannerOptions): ZeroGInstance {
   if (!element) throw new Error('Unable to initialize zero-g because no DOM element was provided');
   if (!element.parentElement) throw new Error('Unable to initialize zero-g because DOM element provided has no parent');
   const instance = new ZeroGInstance(element, options);
